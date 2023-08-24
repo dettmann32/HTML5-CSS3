0ed25df7cf280839c650339b5729523b79c788c4
Deathman <lucasdettmann8@gmail.com>
2023-08-24 12:25:11 -0300

    commit

--- Tailwind/Calculadora/main.js
+++ Tailwind/Calculadora/main.js
@@ -15,7 +15,7 @@ function resolver(){
         }
     }
     
-    // Exemplo de uso
+
     const a = parseFloat( document.getElementById('a').value)
     const b = parseFloat( document.getElementById('b').value)
     const c = parseFloat( document.getElementById('c').value)
